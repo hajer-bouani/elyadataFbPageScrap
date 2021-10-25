f8f5ca488d927a8da0fbf125b14419c287cc08cd
hajer-bouani <hajer.bouani@ensi-uma.tn>
2021-10-25 13:51:33 +0100

    correctbug

--- restartApp.sh
+++ restartApp.sh
@@ -1,5 +1,5 @@
-docker-compose -f f2.yaml down
-h=$(docker images |grep adam | awk '{print $3}')
+docker-compose -f composeFile.yaml down
+h=$(docker images |grep hajerbouani | awk '{print $3}')
 docker rmi $h
-docker build --tag adamlahzami/python-docker:1.1 .
+docker build --tag hajerbouani/python-docker:1.2 .
 
